4f6791d40b3d6c3b33cc8d4c6765f07d217da733
maria lozano <maria.lozano5252@gmail.com>
2020-03-10 17:53:30 -0600

    ya termine perruuuuus

--- examen/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Exámen Segundo Parcial 2019' });
-});
-
-module.exports = router;
